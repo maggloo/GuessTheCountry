af91f8e67958dfc697942fcba34b7e4420932e5a
maggloo <87192791+maggloo@users.noreply.github.com>
2022-05-22 15:43:36 +0300

    Create fireBase.js

---
+++ js/fireBase.js
@@ -0,0 +1,13 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyAMkGgR8go_UXYmn4zUhkgcHaUk8vkfw3w",
+    authDomain: "guessthecountry-b0083.firebaseapp.com",
+    databaseURL: "https://guessthecountry-b0083-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "guessthecountry-b0083",
+    storageBucket: "guessthecountry-b0083.appspot.com",
+    messagingSenderId: "735921944928",
+    appId: "1:735921944928:web:274cfc3ac689440bf38605",
+    measurementId: "G-BGV5FGHLPC"
+});
+
+const myDB = firebase.database();
+const myDBRef = firebase.database().ref();
